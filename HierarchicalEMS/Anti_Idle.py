5ad3de2ecb9fc1fbcd17485c9fc07fc5f03c0289
William Marritt <ak22069@bristol.ac.uk>
2026-03-31 13:33:41 +0100

    complete simulation csv

--- HierarchicalEMS/Anti_Idle.py
+++ HierarchicalEMS/Anti_Idle.py
@@ -4,10 +4,17 @@ import time
 print("Anti-idle script running.")
 
 MOUSEEVENTF_MOVE = 0x0001
+ALT = 0x12
+TAB = 0x09
+
 
 while True:
-    ctypes.windll.user32.mouse_event(MOUSEEVENTF_MOVE, 1, 0, 0, 0)
-    time.sleep(1)
-    ctypes.windll.user32.mouse_event(MOUSEEVENTF_MOVE, -1, 0, 0, 0)
-    time.sleep(59)
+    ctypes.windll.user32.keybd_event(ALT, 0,0,0)
+    ctypes.windll.user32.keybd_event(TAB, 0,0,0)
+    
+    ctypes.windll.user32.keybd_event(ALT, 0,2,0)
+    ctypes.windll.user32.keybd_event(TAB, 0,2,0)
+
+    time.sleep(30)
+    
     
